f8b38f6485fba5801475e96cd79b24d6f55ea80a
fl3xice <two.fl3xice@yandex.ru>
2022-07-02 03:50:44 +0400

    fix exporting

--- mod.ts
+++ mod.ts
@@ -1,5 +1,6 @@
 import { de_DE, en_US, ru_RU } from "./src/Locales.ts";
 
+export { default as Humanity } from "./src/Humanity.ts";
 export { createCustomHumanity, createHumanity } from "./src/Humanity.ts";
 export type { LocaleHumanity } from "./src/Humanity.ts";
 export const DefaultLocales = { de_DE, en_US, ru_RU };
