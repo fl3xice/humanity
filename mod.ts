5148ca1665afac2e022962c1e782c1b98ee149f4
fl3xice <two.fl3xice@yandex.ru>
2022-07-02 02:31:49 +0400

    add mod.ts

---
+++ mod.ts
@@ -0,0 +1,6 @@
+import { de_DE, en_US, ru_RU } from "./src/Locales.ts";
+
+export { createCustomHumanity, createHumanity } from "./src/Humanity.ts";
+export type { LocaleHumanity } from "./src/Humanity.ts";
+export const DefaultLocales = { de_DE, en_US, ru_RU };
+export type { DeclinationLocale, LocaleObject } from "./src/Locales.ts";
